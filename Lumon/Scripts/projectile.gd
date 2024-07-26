92aa203f8fc09a2bcd97948ccf37fff89c653a84
Dylan Carlyle <diagonalcorgi@gmail.com>
2024-07-26 16:31:38 +1200

    fixed portal gun projectiles having funny physics

--- Lumon/Scripts/projectile.gd
+++ Lumon/Scripts/projectile.gd
@@ -1,30 +1,28 @@
 extends CharacterBody2D
+@onready var timer = $Timer
 
 # Speed of the projectile
 var speed = 400
 # Direction the projectile is moving in
 var direction = Vector2.RIGHT
+var enemy_contact = false
 
 func _ready():
 	# Set the initial direction (you can set this dynamically when instantiating the projectile)
 	direction = direction.normalized()
+	timer.wait_time = 2.0
+	timer.start()
 
 func _physics_process(delta):
 	# Move the projectile
 	var velocity = direction * speed * delta
 	var collision = move_and_collide(velocity)
-	
-	if collision:
-		# Handle collision (e.g., destroy the projectile, deal damage)
-		#queue_free()
-		pass
 
 # Set the direction based on an angle (in radians)
 func set_direction_angle(angle):
 	direction = Vector2(cos(angle), sin(angle)).normalized()
 
 func _on_area_2d_area_entered(area):
-	print("teleport")
 	if(area.is_in_group("portal")):
 		if(!area.lock_portal):
 			do_teleport(area)
@@ -37,3 +35,8 @@ func do_teleport(area):
 					#area.lock_portal = true
 					area.do_lock()
 					global_position = portal.global_position
+
+
+func _on_timer_timeout():
+	print("timeout")
+	queue_free()
