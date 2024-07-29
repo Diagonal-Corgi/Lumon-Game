4ab0246057f0fb4ecf5cd202d38e7c3192d338e4
Dylan Carlyle <diagonalcorgi@gmail.com>
2024-07-29 13:48:12 +1200

    added dialogic

--- Lumon/Scripts/Sign_Button.gd
+++ Lumon/Scripts/Sign_Button.gd
@@ -1,5 +1,5 @@
 extends Button
-
+@onready var audio_stream_player = $AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +12,5 @@ func _process(_delta):
 
 
 func _on_pressed():
+	audio_stream_player.play()
 	get_tree().change_scene_to_file("res://Scenes/game_1.tscn")
--- Lumon/Scripts/StartButton.gd
+++ Lumon/Scripts/StartButton.gd
@@ -1,5 +1,8 @@
 extends Button
+@onready var audio_stream_player = $AudioStreamPlayer
 
 
 func _on_pressed():
+	audio_stream_player.play()
 	get_tree().change_scene_to_file("res://Scenes/onboarding.tscn")
+
